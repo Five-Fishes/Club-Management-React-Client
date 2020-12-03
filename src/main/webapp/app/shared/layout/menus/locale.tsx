c860747456a04ed9b05db00b6d3273e32d974ccd
Ka Chun <34387770+kachun333@users.noreply.github.com>
2020-12-03 15:59:32 +0800

    change language icon

--- src/main/webapp/app/shared/layout/menus/locale.tsx
+++ src/main/webapp/app/shared/layout/menus/locale.tsx
@@ -5,7 +5,7 @@ import { locales, languages } from 'app/config/translation';
 
 export const LocaleMenu = ({ currentLocale, onClick }) =>
   Object.keys(languages).length > 1 && (
-    <NavDropdown icon="flag" name={currentLocale ? languages[currentLocale].name : undefined}>
+    <NavDropdown icon="globe" name={currentLocale ? languages[currentLocale].name : undefined}>
       {locales.map(locale => (
         <DropdownItem key={locale} value={locale} onClick={onClick}>
           {languages[locale].name}
