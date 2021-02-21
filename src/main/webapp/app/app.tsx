073ed5b9ca9043d3a5bcd3625c3e48a63aa2a97b
Ka Chun <34387770+kachun333@users.noreply.github.com>
2021-02-21 20:15:40 +0800

    change app layout css
    remove ugly jhipster generated card

--- src/main/webapp/app/app.tsx
+++ src/main/webapp/app/app.tsx
@@ -47,12 +47,10 @@ export class App extends React.Component<IAppProps> {
               isSwaggerEnabled={this.props.isSwaggerEnabled}
             />
           </ErrorBoundary>
-          <div className="container-fluid view-container" id="app-view-container">
-            <Card className="jh-card">
-              <ErrorBoundary>
-                <AppRoutes />
-              </ErrorBoundary>
-            </Card>
+          <div className="view-container">
+            <ErrorBoundary>
+              <AppRoutes />
+            </ErrorBoundary>
           </div>
           <Footer />
         </div>
