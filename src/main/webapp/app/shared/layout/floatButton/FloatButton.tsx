fe06e7abf1e8534d2083d03eed8e93937bcb7896
Clement Saw Kiat Chun <cskiat06@gmail.com>
2021-02-26 00:28:05 +0800

    Refactor floatButton onClick

--- src/main/webapp/app/shared/layout/floatButton/FloatButton.tsx
+++ src/main/webapp/app/shared/layout/floatButton/FloatButton.tsx
@@ -6,15 +6,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './floatButton.css';
 
 const FloatButton = props => {
-  console.log(props);
   return (
-    <Button
-      color="action"
-      className="floatButton shadow"
-      onClick={() => {
-        props.onClick();
-      }}
-    >
+    <Button color="action" className="floatButton shadow" onClick={props.onClick}>
       <FontAwesomeIcon icon="plus" size="lg" />
     </Button>
   );
