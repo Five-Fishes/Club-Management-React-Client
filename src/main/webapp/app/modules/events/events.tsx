100edca556b589db0faaf5368f60a7c7468ffd3e
Clement Saw Kiat Chun <cskiat06@gmail.com>
2021-02-28 14:59:32 +0800

    Refactor the EventCard as components

--- src/main/webapp/app/modules/events/events.tsx
+++ src/main/webapp/app/modules/events/events.tsx
@@ -39,31 +39,33 @@ const dummy: IEvent[] = [
   }
 ];
 
+const EventCard = ({ event }) => (
+  <Card className="p-4">
+    <Row>
+      <Col xs="4" className="pr-0">
+        <CardImg height="100%" width="100%" src={event.imageUrl} alt="Event image" />
+      </Col>
+      <Col xs="8">
+        <Button color="link" className="option-icon p-0">
+          <FontAwesomeIcon icon={faEllipsisH} />
+        </Button>
+        <div className="my-auto">
+          <h4>{event.name}</h4>
+          <p className="mb-0">Date: {event.startDate.format('DD MMM YYYY')}</p>
+          <p className="mb-0">Time: {event.startDate.format('h:mm a')}</p>
+          <p className="mb-0">Venue: {event.venue}</p>
+        </div>
+      </Col>
+    </Row>
+  </Card>
+);
+
 export class Events extends React.Component {
-  renderedEvents = dummy.map(event => {
-    return (
-      <div className="my-3" key={event.id}>
-        <Card className="p-4">
-          <Row>
-            <Col xs="4" className="pr-0">
-              <CardImg height="100%" width="100%" src={event.imageUrl} alt="Event image" />
-            </Col>
-            <Col xs="8">
-              <Button color="link" className="option-icon p-0">
-                <FontAwesomeIcon icon={faEllipsisH} />
-              </Button>
-              <div className="my-auto">
-                <h4>{event.name}</h4>
-                <p className="mb-0">Date: {event.startDate.format('DD MMM YYYY')}</p>
-                <p className="mb-0">Time: {event.startDate.format('h:mm a')}</p>
-                <p className="mb-0">Venue: {event.venue}</p>
-              </div>
-            </Col>
-          </Row>
-        </Card>
-      </div>
-    );
-  });
+  renderedEvents = dummy.map(event => (
+    <div className="my-3" key={event.id}>
+      <EventCard event={event} />
+    </div>
+  ));
 
   render() {
     return (
