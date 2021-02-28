ec1fa43fdabbcb1d5484f0b321e05613b1e169ef
HowardYaw <yjhhoward@gmail.com>
2021-02-28 19:04:51 +0800

    listing card with action menu and title, dynamic children

--- src/main/webapp/app/shared/components/listing-card/listing-card.tsx
+++ src/main/webapp/app/shared/components/listing-card/listing-card.tsx
@@ -1,10 +1,38 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
-import {} from 'reactstrap';
+import { Row, Col } from 'reactstrap';
+import './listing-card.scss';
 
-export interface IListingCardProps {}
+export interface IListingCardProps {
+  showActionMenu: boolean;
+  actionMenuHandler?: Function;
+  title?: string;
+}
 
 export class ListingCard extends React.Component<IListingCardProps> {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
-    return <div />;
+    return (
+      <div className="card-container container">
+        <Row className="align-items-center justify-content-center">
+          <Col xs="10">
+            {this.props.title && this.props.title.hasOwnProperty('value') ? (
+              <span className="font-weight-bold text-dark d-block mb-1">{this.props.title}</span>
+            ) : null}
+            {this.props.children}
+          </Col>
+          <Col xs="1">
+            {this.props.showActionMenu ? (
+              <span onClick={this.props.actionMenuHandler()} className="hand">
+                <FontAwesomeIcon icon="ellipsis-v" />
+              </span>
+            ) : null}
+          </Col>
+        </Row>
+      </div>
+    );
   }
 }
