9268281b5069ac50433d86a8c66570d54b8064f4
Ka Chun <34387770+kachun333@users.noreply.github.com>
2021-03-06 11:59:12 +0800

    Update axios-interceptor.ts
    only 401 status code will auto logout user

--- src/main/webapp/app/config/axios-interceptor.ts
+++ src/main/webapp/app/config/axios-interceptor.ts
@@ -1,15 +1,15 @@
 import axios from 'axios';
 import { getBasePath, Storage } from 'react-jhipster';
 
-import { SERVER_API_URL } from 'app/config/constants';
+import { SERVER_API_URL, AUTH_TOKEN_KEY } from 'app/config/constants';
 
 const TIMEOUT = 1 * 60 * 1000;
 axios.defaults.timeout = TIMEOUT;
 axios.defaults.baseURL = SERVER_API_URL;
 
-const setupAxiosInterceptors = onUnauthenticated => {
+function setupAxiosInterceptors(onUnauthenticated: () => void) {
   const onRequestSuccess = config => {
-    const token = Storage.local.get('jhi-authenticationToken') || Storage.session.get('jhi-authenticationToken');
+    const token = Storage.local.get(AUTH_TOKEN_KEY) || Storage.session.get(AUTH_TOKEN_KEY);
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
@@ -18,13 +18,13 @@ const setupAxiosInterceptors = onUnauthenticated => {
   const onResponseSuccess = response => response;
   const onResponseError = err => {
     const status = err.status || (err.response ? err.response.status : 0);
-    if (status === 403 || status === 401) {
+    if (status === 401) {
       onUnauthenticated();
     }
     return Promise.reject(err);
   };
   axios.interceptors.request.use(onRequestSuccess);
   axios.interceptors.response.use(onResponseSuccess, onResponseError);
-};
+}
 
 export default setupAxiosInterceptors;
