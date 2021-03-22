c55a76a4344050d56cae7f4dfaf1d30e8264c299
LUXIANZE <luxianze98@gmail.com>
2021-03-22 20:34:30 +0800

    lint fix

--- src/main/webapp/app/shared/layout/menus/modules.tsx
+++ src/main/webapp/app/shared/layout/menus/modules.tsx
@@ -2,52 +2,57 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { NavLink as Link } from 'react-router-dom';
 
-export const ModuleMenu = ({ button }) => (
-  <div
-    className="h-100"
-    style={{
-      left: 0,
-      top: 0,
-      zIndex: 1001,
-      height: '100vh',
-      width: '100%',
-      position: 'fixed'
-    }}
-  >
+export const ModuleMenu = ({ button }) => {
+  const handler = () => {
+    button.props.onClick();
+  };
+  return (
     <div
+      className="h-100"
       style={{
-        background: 'black',
-        opacity: 0.5,
-        height: '100vh'
-      }}
-      onClick={button.props.onClick}
-    />
-    <div
-      style={{
-        background: '#07ADE1',
         left: 0,
         top: 0,
-        position: 'absolute',
+        zIndex: 1001,
         height: '100vh',
-        padding: '12px 16px',
-        minWidth: 325,
-        maxWidth: 325,
-        display: 'flex',
-        flexDirection: 'column'
+        width: '100%',
+        position: 'fixed'
       }}
     >
-      <div style={{ display: 'flex' }}>{button}</div>
+      <div
+        style={{
+          background: 'black',
+          opacity: 0.5,
+          height: '100vh'
+        }}
+        onClick={handler}
+      />
+      <div
+        style={{
+          background: '#07ADE1',
+          left: 0,
+          top: 0,
+          position: 'absolute',
+          height: '100vh',
+          padding: '12px 16px',
+          minWidth: 325,
+          maxWidth: 325,
+          display: 'flex',
+          flexDirection: 'column'
+        }}
+      >
+        <div style={{ display: 'flex' }}>{button}</div>
 
-      <div style={{ paddingTop: 30, paddingBottom: 30 }}>
-        <Link to="/entity/event" onClick={button.props.onClick}>
-          <div style={{ height: 50, width: '100%', padding: '0px 18px', color: 'white' }}>
-            <FontAwesomeIcon icon="user" />
-            Event
-          </div>
-        </Link>
+        <div style={{ paddingTop: 30, paddingBottom: 30 }}>
+          <Link to="/entity/event" onClick={handler}>
+            <div style={{ height: 50, width: '100%', padding: '0px 18px', color: 'white' }}>
+              <FontAwesomeIcon icon="user" />
+              Event
+            </div>
+          </Link>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default ModuleMenu;
