81634eedbd8296ca9d41d3479170f09a94087e1b
Clement <cskiat06@gmail.com>
2021-05-11 15:56:41 +0800

    Init commit

--- src/main/webapp/app/entities/event/event.tsx
+++ src/main/webapp/app/entities/event/event.tsx
@@ -133,40 +133,40 @@ const EventCard: React.FC<IEventCardProps> = ({ event, toggleModal }) => {
   const onToggleModal = () => toggleModal(event.id);
   return (
     <Card className="p-3 pt-4 event-card">
-      <Row>
-        <Col xs="4" lg="5" className="pr-0">
-          <CardImg
-            height="100%"
-            width="100%"
-            className="rounded-0"
-            src={event.imageUrl ? event.imageUrl : 'content/images/placeholder.png'}
-            alt={event.fileName}
-          />
-        </Col>
-        <Col xs="8" lg="7">
-          <AuthorizationChecker ccRole={CCRole.ADMIN} eventRole={EventRole.HEAD} eventId={event.id}>
-            <Button color="link" className="option-icon p-0" onClick={onToggleModal}>
-              <FontAwesomeIcon icon={'ellipsis-h'} />
-            </Button>
-          </AuthorizationChecker>
-          <div className="my-auto">
-            <Link to={`/entity/event/${event.id}`}>
+      <AuthorizationChecker ccRole={CCRole.ADMIN} eventRole={EventRole.HEAD} eventId={event.id}>
+        <Button color="link" className="option-icon p-0" onClick={onToggleModal}>
+          <FontAwesomeIcon icon={'ellipsis-h'} />
+        </Button>
+      </AuthorizationChecker>
+      <Link to={`/entity/event/${event.id}`}>
+        <Row>
+          <Col xs="4" lg="5" className="pr-0">
+            <CardImg
+              height="100%"
+              width="100%"
+              className="rounded-0"
+              src={event.imageUrl ? event.imageUrl : 'content/images/placeholder.png'}
+              alt={event.fileName}
+            />
+          </Col>
+          <Col xs="8" lg="7">
+            <div className="my-auto">
               <h4 className="event-title">{event.name}</h4>
-            </Link>
-            <p className="mb-0">
-              <Translate contentKey="clubmanagementApp.event.startDate">Start Date</Translate>:{' '}
-              <TextFormat type="date" value={event.startDate} format={APP_DATE_12_ABR_FORMAT} />
-            </p>
-            <p className="mb-0">
-              <Translate contentKey="clubmanagementApp.event.endDate">End Date</Translate>:{' '}
-              <TextFormat type="date" value={event.endDate} format={APP_DATE_12_ABR_FORMAT} />
-            </p>
-            <p className="mb-0">
-              <Translate contentKey="clubmanagementApp.event.venue">Venue</Translate>: {event.venue}
-            </p>
-          </div>
-        </Col>
-      </Row>
+              <p className="mb-0">
+                <Translate contentKey="clubmanagementApp.event.startDate">Start Date</Translate>:{' '}
+                <TextFormat type="date" value={event.startDate} format={APP_DATE_12_ABR_FORMAT} />
+              </p>
+              <p className="mb-0">
+                <Translate contentKey="clubmanagementApp.event.endDate">End Date</Translate>:{' '}
+                <TextFormat type="date" value={event.endDate} format={APP_DATE_12_ABR_FORMAT} />
+              </p>
+              <p className="mb-0">
+                <Translate contentKey="clubmanagementApp.event.venue">Venue</Translate>: {event.venue}
+              </p>
+            </div>
+          </Col>
+        </Row>
+      </Link>
     </Card>
   );
 };
