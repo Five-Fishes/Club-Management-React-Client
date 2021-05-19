580e64d91293ae6126b32789977e9311d0dac491
HowardYaw <yjhhoward@gmail.com>
2021-05-19 22:09:32 +0800

    update convert server date util

--- src/main/webapp/app/shared/util/date-utils.ts
+++ src/main/webapp/app/shared/util/date-utils.ts
@@ -1,6 +1,6 @@
 import moment from 'moment';
 
-import { APP_LOCAL_DATETIME_FORMAT, APP_LOCAL_DATETIME_FORMAT_Z } from 'app/config/constants';
+import { APP_LOCAL_DATETIME_FORMAT, APP_LOCAL_DATETIME_FORMAT_Z, APP_LOCAL_DATE_FORMAT } from 'app/config/constants';
 
 export function convertDateTimeFromServer(date?: moment.MomentInput): string | null {
   if (!date) return null;
@@ -14,10 +14,12 @@ export function convertDateTimeToServer(date?: moment.MomentInput): Date | null 
 
 export function convertDateTimeFromServerToLocaleDate(date?: moment.MomentInput): string {
   if (!date) return '';
-  return new Date(date.toLocaleString()).toLocaleDateString();
+  date = new Date(date.toLocaleString());
+  return moment(date).format(APP_LOCAL_DATE_FORMAT);
 }
 
 export function convertDateTimeFromServerToLocaleDateTime(date?: moment.MomentInput): string {
   if (!date) return '';
-  return new Date(date.toLocaleString()).toLocaleString();
+  date = new Date(date.toLocaleString());
+  return moment(date).format(APP_LOCAL_DATETIME_FORMAT);
 }
