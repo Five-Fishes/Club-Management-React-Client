0de99f0d4766981fbf6225b61c3861a2bf83577a
HowardYaw <yjhhoward@gmail.com>
2021-07-31 12:42:24 +0800

    update club family reducer function

--- src/main/webapp/app/entities/club-family/club-family.reducer.ts
+++ src/main/webapp/app/entities/club-family/club-family.reducer.ts
@@ -6,14 +6,11 @@ import { REQUEST, SUCCESS, FAILURE } from 'app/shared/reducers/action-type.util'
 
 import { IClubFamily, defaultValue } from 'app/shared/model/club-family.model';
 import { AnyAction } from 'redux';
+import { getClubFamilyDetails, getClubFamilyList } from 'app/shared/services/club-family-info.service';
 
 export const ACTION_TYPES = {
   FETCH_CLUBFAMILY_LIST: 'clubFamily/FETCH_CLUBFAMILY_LIST',
   FETCH_CLUBFAMILY: 'clubFamily/FETCH_CLUBFAMILY',
-  CREATE_CLUBFAMILY: 'clubFamily/CREATE_CLUBFAMILY',
-  UPDATE_CLUBFAMILY: 'clubFamily/UPDATE_CLUBFAMILY',
-  DELETE_CLUBFAMILY: 'clubFamily/DELETE_CLUBFAMILY',
-  SET_BLOB: 'clubFamily/SET_BLOB',
   RESET: 'clubFamily/RESET',
 };
 
@@ -39,71 +36,17 @@ export interface IClubFamilyState {
 
 export default (state: IClubFamilyState = initialState, action: AnyAction): IClubFamilyState => {
   switch (action.type) {
-    case REQUEST(ACTION_TYPES.FETCH_CLUBFAMILY_LIST):
-    case REQUEST(ACTION_TYPES.FETCH_CLUBFAMILY):
-      return {
-        ...state,
-        errResponse: null,
-        updateSuccess: false,
-        loading: true,
-      };
-    case REQUEST(ACTION_TYPES.CREATE_CLUBFAMILY):
-    case REQUEST(ACTION_TYPES.UPDATE_CLUBFAMILY):
-    case REQUEST(ACTION_TYPES.DELETE_CLUBFAMILY):
-      return {
-        ...state,
-        errResponse: null,
-        updateSuccess: false,
-        updating: true,
-      };
-    case FAILURE(ACTION_TYPES.FETCH_CLUBFAMILY_LIST):
-    case FAILURE(ACTION_TYPES.FETCH_CLUBFAMILY):
-    case FAILURE(ACTION_TYPES.CREATE_CLUBFAMILY):
-    case FAILURE(ACTION_TYPES.UPDATE_CLUBFAMILY):
-    case FAILURE(ACTION_TYPES.DELETE_CLUBFAMILY):
+    case ACTION_TYPES.FETCH_CLUBFAMILY_LIST:
       return {
         ...state,
         loading: false,
-        updating: false,
-        updateSuccess: false,
-        errResponse: action.payload,
+        entities: action.payload,
       };
-    case SUCCESS(ACTION_TYPES.FETCH_CLUBFAMILY_LIST):
+    case ACTION_TYPES.FETCH_CLUBFAMILY:
       return {
         ...state,
         loading: false,
-        entities: action.payload.data,
-      };
-    case SUCCESS(ACTION_TYPES.FETCH_CLUBFAMILY):
-      return {
-        ...state,
-        loading: false,
-        entity: action.payload.data,
-      };
-    case SUCCESS(ACTION_TYPES.CREATE_CLUBFAMILY):
-    case SUCCESS(ACTION_TYPES.UPDATE_CLUBFAMILY):
-      return {
-        ...state,
-        updating: false,
-        updateSuccess: true,
-        entity: action.payload.data,
-      };
-    case SUCCESS(ACTION_TYPES.DELETE_CLUBFAMILY):
-      return {
-        ...state,
-        updating: false,
-        updateSuccess: true,
-        entity: {},
-      };
-    case ACTION_TYPES.SET_BLOB:
-      const { name, data, contentType } = action.payload;
-      return {
-        ...state,
-        entity: {
-          ...state.entity,
-          [name]: data,
-          [name + 'ContentType']: contentType,
-        },
+        entity: action.payload,
       };
     case ACTION_TYPES.RESET:
       return {
@@ -114,58 +57,16 @@ export default (state: IClubFamilyState = initialState, action: AnyAction): IClu
   }
 };
 
-const apiUrl = 'api/club-families';
-
 // Actions
 
-export const getEntities: ICrudGetAllAction<IClubFamily> = (page, size, sort) => ({
+export const getEntities: any = () => ({
   type: ACTION_TYPES.FETCH_CLUBFAMILY_LIST,
-  payload: axios.get<IClubFamily>(`${apiUrl}?cacheBuster=${new Date().getTime()}`),
+  payload: getClubFamilyList(),
 });
 
-export const getEntity: ICrudGetAction<IClubFamily> = id => {
-  const requestUrl = `${apiUrl}/${id}`;
-  return {
-    type: ACTION_TYPES.FETCH_CLUBFAMILY,
-    payload: axios.get<IClubFamily>(requestUrl),
-  };
-};
-
-export const createEntity: ICrudPutAction<IClubFamily> = entity => async dispatch => {
-  const result = await dispatch({
-    type: ACTION_TYPES.CREATE_CLUBFAMILY,
-    payload: axios.post(apiUrl, cleanEntity(entity)),
-  });
-  dispatch(getEntities());
-  return result;
-};
-
-export const updateEntity: ICrudPutAction<IClubFamily> = entity => async dispatch => {
-  const result = await dispatch({
-    type: ACTION_TYPES.UPDATE_CLUBFAMILY,
-    payload: axios.put(apiUrl, cleanEntity(entity)),
-  });
-  dispatch(getEntities());
-  return result;
-};
-
-export const deleteEntity: ICrudDeleteAction<IClubFamily> = id => async dispatch => {
-  const requestUrl = `${apiUrl}/${id}`;
-  const result = await dispatch({
-    type: ACTION_TYPES.DELETE_CLUBFAMILY,
-    payload: axios.delete(requestUrl),
-  });
-  dispatch(getEntities());
-  return result;
-};
-
-export const setBlob = (name: any, data: any, contentType?: any) => ({
-  type: ACTION_TYPES.SET_BLOB,
-  payload: {
-    name,
-    data,
-    contentType,
-  },
+export const getEntity: any = (code: string) => ({
+  type: ACTION_TYPES.FETCH_CLUBFAMILY,
+  payload: getClubFamilyDetails(code),
 });
 
 export const reset = () => ({
--- src/main/webapp/app/shared/services/club-family-info.service.ts
+++ src/main/webapp/app/shared/services/club-family-info.service.ts
@@ -1,28 +1,54 @@
 import { IClubFamily } from '../model/club-family.model';
 
+// Properties are set with i18n key so that can toggle value between languages
+// Code as Identifies is using exact value
 export function getClubFamilyDetails(clubFamilyCode: string): IClubFamily {
   const clubFamilyDetails: IClubFamily = {};
   clubFamilyDetails.code = clubFamilyCode;
   if (clubFamilyCode === 'BI_MU') {
-    clubFamilyDetails.name = 'Bi Mu';
-    clubFamilyDetails.slogan = 'Bi Mu Bi Mu, No Dumb than you';
-    clubFamilyDetails.description = 'Bi Mu Family is famous for its smart and interesting fishes';
+    clubFamilyDetails.name = 'clubmanagementApp.clubFamily.bimu.name';
+    clubFamilyDetails.slogan = 'clubmanagementApp.clubFamily.bimu.slogan';
+    clubFamilyDetails.description = 'clubmanagementApp.clubFamily.bimu.description';
   } else if (clubFamilyCode === 'JIN_LONG') {
-    clubFamilyDetails.name = 'Jin Long';
-    clubFamilyDetails.slogan = 'Jin Long Spirit, Leng Zai Leng Nui';
-    clubFamilyDetails.description = 'Jin Long Family is famous for its Leng Zai Leng lui';
+    clubFamilyDetails.name = 'clubmanagementApp.clubFamily.jinlong.name';
+    clubFamilyDetails.slogan = 'clubmanagementApp.clubFamily.jinlong.slogan';
+    clubFamilyDetails.description = 'clubmanagementApp.clubFamily.jinlong.description';
   } else if (clubFamilyCode === 'KONG_QUE') {
-    clubFamilyDetails.name = 'Kong Que';
-    clubFamilyDetails.slogan = 'We are better than the best among the best of the best';
-    clubFamilyDetails.description = 'Peacock Family Fishes are the best of the best';
+    clubFamilyDetails.name = 'clubmanagementApp.clubFamily.kongque.name';
+    clubFamilyDetails.slogan = 'clubmanagementApp.clubFamily.kongque.slogan';
+    clubFamilyDetails.description = 'clubmanagementApp.clubFamily.kongque.description';
   } else if (clubFamilyCode === 'QI_CAI') {
-    clubFamilyDetails.name = 'Qi Cai';
-    clubFamilyDetails.slogan = 'Qi Cai Qi Cai, Step on You';
-    clubFamilyDetails.description = 'Qi Cai Family Fishes are friendly and truth personality';
+    clubFamilyDetails.name = 'clubmanagementApp.clubFamily.qicai.name';
+    clubFamilyDetails.slogan = 'clubmanagementApp.clubFamily.qicai.slogan';
+    clubFamilyDetails.description = 'clubmanagementApp.clubFamily.qicai.description';
   } else if (clubFamilyCode === 'XIAO_CHOU') {
-    clubFamilyDetails.name = 'Xiao Chou';
-    clubFamilyDetails.slogan = 'Nemo Nemo, No More Emo';
-    clubFamilyDetails.description = 'Nemo Family Fishes always full with positive energy and make you away from Emo';
+    clubFamilyDetails.name = 'clubmanagementApp.clubFamily.xiaochou.name';
+    clubFamilyDetails.slogan = 'clubmanagementApp.clubFamily.xiaochou.slogan';
+    clubFamilyDetails.description = 'clubmanagementApp.clubFamily.xiaochou.description';
   }
   return clubFamilyDetails;
+}
+
+export function getClubFamilyList(): IClubFamily[] {
+  const biMuFamily: IClubFamily = {
+    code: 'BI_MU',
+    name: 'clubmanagementApp.clubFamily.bimu.name',
+  };
+  const jinLongFamily: IClubFamily = {
+    code: 'JIN_LONG',
+    name: 'clubmanagementApp.clubFamily.jinlong.name',
+  };
+  const kongQueFamily: IClubFamily = {
+    code: 'KONG_QUE',
+    name: 'clubmanagementApp.clubFamily.kongque.name',
+  };
+  const qiCaiFamily: IClubFamily = {
+    code: 'QI_CAI',
+    name: 'clubmanagementApp.clubFamily.qicai.name',
+  };
+  const xiaoChouFamily: IClubFamily = {
+    code: 'XIAO_CHOU',
+    name: 'clubmanagementApp.clubFamily.xiaochou.name',
+  };
+  return [biMuFamily, jinLongFamily, kongQueFamily, qiCaiFamily, xiaoChouFamily];
 }
